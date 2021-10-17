fedbb9f22ceea8b9b1d95779b486d9591174ac64
Indraan S. Toor <indraantoor@gmail.com>
2021-10-17 20:14:17 +0530

    Add: csv parser function

--- server/index.js
+++ server/index.js
@@ -6,6 +6,41 @@ const connection = require("./db");
 const users = require("./routes/users");
 const projects = require("./routes/projects");
 const models = require("./routes/models");
+const csvtojson = require("csvtojson");
+const fs = require("fs");
+const syntheticData = require("./models/syntheticData");
+const { parse } = require("path");
+const mongoose = require("mongoose");
+
+// CONVERT TO CSV
+const csvfilepath = "myfile.csv";
+var temp;
+
+csvtojson()
+  .fromFile(csvfilepath)
+  .then((json) => {
+    // console.log(json);
+    // fs.writeFileSync("output.json", JSON.stringify(json), "utf-8", (err) => {
+    //   if (err) console.log(err);
+    // });
+    for (var x = 0; x < json; x++) {
+      temp = parseInt(json[x].CLIENTNUM);
+      json[x].CLIENTNUM = temp;
+      temp = parse(json[x].Attrition_Flag);
+      json[x].Attrition_Flag = temp;
+    }
+
+    syntheticData.insertMany({ data: json }, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("ok");
+      }
+    });
+  });
+
+const data = fs.readFileSync("output.json", "utf-8");
+// console.log(data);
 
 // Database Connection
 connection();
---
+++ server/models/syntheticData.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const test = mongoose.Schema(
+  {
+    CLIENTNUM: Number,
+    Attrition_Flag: String,
+    Customer_Age: Number,
+    Gender: String,
+    Dependent_count: Number,
+    Education_Level: String,
+    Marital_Status: String,
+    Income_Category: String,
+    Card_Category: String,
+    Months_on_book: Number,
+    Total_Relationship_Count: Number,
+    Months_Inactive_12_mon: Number,
+    Contacts_Count_12_mon: Number,
+    Credit_Limit: Number,
+    Total_Revolving_Bal: Number,
+    Avg_Open_To_Buy: Number,
+    Total_Amt_Chng_Q4_Q1: Number,
+    Total_Trans_Amt: Number,
+    Total_Trans_Ct: Number,
+    Total_Ct_Chng_Q4_Q1: Number,
+    Avg_Utilization_Ratio: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const syntheticDataSchema = mongoose.Schema({
+  data: [test],
+});
+
+const SyntheticData = mongoose.model("syntheticData", syntheticDataSchema);
+module.exports = SyntheticData;
