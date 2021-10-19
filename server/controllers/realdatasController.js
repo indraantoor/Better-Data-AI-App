b8abf8fda95adb15cef90b0bfcb3239b179d25b2
Indraan S. Toor <indraantoor@gmail.com>
2021-10-19 05:52:39 +0530

    Refactor: real data functions in their own controller

---
+++ server/controllers/realdatasController.js
@@ -0,0 +1,48 @@
+const {
+  RealData,
+  realDataValidator,
+  realDataUpdateValidator,
+} = require("../models/realData");
+
+const getParticularRealData = async (req, res) => {
+  const realdata = await RealData.findById(req.params.id);
+  res.send(realdata);
+};
+
+// Gives all real data files under that particular project
+const getAllRealDatasByProject = async (req, res) => {
+  const projectId = req.body.Project_id;
+  const realdatas = await RealData.find({ Project_id: projectId }).populate(
+    "Project_id"
+  );
+  res.send(realdatas);
+};
+
+// Handles Updating
+const update = async (req, res) => {
+  const realdata = await RealData.findById(req.params.id);
+  if (realdata.User_id.toString() !== req.user.id.toString()) {
+    res.status(401).send("Not authorized");
+    throw new Error("Not authorized");
+  }
+  await RealData.findByIdAndUpdate({ _id: req.params.id }, { $set: req.body });
+  res.status(200).send("Real Data Name Updated Successfully");
+};
+
+// Deletes that real data csv file's which was stored in the database
+const deleteRealDataCsv = async (req, res) => {
+  const realdata = await RealData.findById(req.params.id);
+  if (realdata.User_id.toString() !== req.user.id.toString()) {
+    res.status(401).send("Not authorized");
+    throw new Error("Not authorized");
+  }
+  await RealData.findByIdAndDelete(req.params.id);
+  res.status(200).json("Real Data deleted successfully");
+};
+
+module.exports = {
+  update,
+  getAllRealDatasByProject,
+  getParticularRealData,
+  deleteRealDataCsv,
+};
--- server/routes/realdatas.js
+++ server/routes/realdatas.js
@@ -10,74 +10,36 @@ const mongoose = require("mongoose");
 const authenToken = require("../middleware/authToken");
 const validate = require("../middleware/validate");
 const isValidObjectId = require("../middleware/isValidObjectId");
+const {
+  update,
+  getAllRealDatasByProject,
+  getParticularRealData,
+  deleteRealDataCsv,
+} = require("../controllers/realdatasController");
 
 // Get all csv files
 router.get(
   "/",
-  asyncHandler(async (req, res) => {
-    const projectId = req.body.Project_id;
-    const realdatas = await RealData.find({ Project_id: projectId }).populate(
-      "Project_id"
-    );
-    res.send(realdatas);
+  asyncHandler((req, res) => {
+    getAllRealDatasByProject(req, res);
   })
 );
-
-// var realDataUpdateSchema = {
-//   "data.$.CLIENTNUM": req.body.CLIENTNUM,
-//   "data.$.Attrition_Flag": req.body.Attrition_Flag,
-//   "data.$.Customer_Age": req.body.Customer_Age,
-//   "data.$.Gender": req.body.Gender,
-//   "data.$.Dependent_count": req.body.Dependent_count,
-//   "data.$.Education_Level": req.body.Education_Level,
-// };
-
-// // Updates real data csv file data
-// router.put(
-//   "/:id",
-//   // [isValidObjectId, validate(modelValidator)],
-//   // isValidObjectId,
-//   asyncHandler(async (req, res) => {
-//     // await realData.findByIdAndUpdate(
-//     //   { _id: req.params.id },
-//     //   { $set: req.body }
-//     // );
-//     // const obj = { ...req.body };
-//     var id = mongoose.Types.ObjectId(req.params.id);
-//     var id2 = mongoose.Types.ObjectId(req.body.main);
-//     await realData.updateOne(
-//       { _id: id2, "data._id": id },
-//       { $set: { "data.$.Gender": req.body.Gender } }
-//     );
-//     res.status(200).send("Real Data Updated Successfully");
-//   })
-// );
 
 // Update Real Data Name
 router.put(
   "/:id",
   [isValidObjectId, authenToken, validate(realDataUpdateValidator)],
-  asyncHandler(async (req, res) => {
-    const realdata = await RealData.findById(req.params.id);
-    if (realdata.User_id.toString() !== req.user.id.toString()) {
-      res.status(401).send("Not authorized");
-      throw new Error("Not authorized");
-    }
-    await RealData.findByIdAndUpdate(
-      { _id: req.params.id },
-      { $set: req.body }
-    );
-    res.status(200).send("Real Data Name Updated Successfully");
+  asyncHandler((req, res) => {
+    update(req, res);
   })
 );
 
 // Get Real Data By Id
 router.get(
   "/:id",
-  // isValidObjectId,
-  asyncHandler(async (req, res) => {
-    const realdata = await RealData.findById(req.params.id);
-    res.send(realdata);
+  isValidObjectId,
+  asyncHandler((req, res) => {
+    getParticularRealData(req, res);
   })
 );
 
@@ -85,14 +47,8 @@ router.get(
 router.delete(
   "/:id",
   [isValidObjectId, authenToken],
-  asyncHandler(async (req, res) => {
-    const realdata = await RealData.findById(req.params.id);
-    if (realdata.User_id.toString() !== req.user.id.toString()) {
-      res.status(401).send("Not authorized");
-      throw new Error("Not authorized");
-    }
-    await RealData.findByIdAndDelete(req.params.id);
-    res.status(200).json("Real Data deleted successfully");
+  asyncHandler((req, res) => {
+    deleteRealDataCsv(req, res);
   })
 );
 
